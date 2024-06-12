2c41f53b67815bd2d790eaa9d87b41b2f0a7e658
Ashish Srivastava <ashish@bluelupin.com>
2024-06-12 23:22:03 +0530

    Fixes

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  apps: [
-    {
-      name: 'opnform',
-      port: '3100',
-      exec_mode: 'cluster',
-      instances: 'max',
-      script: './.output/server/index.mjs'
-    }
-  ]
+  apps : [{
+    name      : 'opnform',
+    script    : './.output/server/index.mjs',
+    instances : 'max',
+    exec_mode : 'cluster',
+    watch     : false,
+    port      : 3100
+  }]
 }
